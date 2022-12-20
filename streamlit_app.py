f1eee94c61dc66990a5317cbdac50de1e13f4a7b
tesevaco <120709697+tesevaco@users.noreply.github.com>
2022-12-20 09:50:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,7 @@ try:
                  streamlit.error("Please select a fruit to get information.")
     else:
                  back_from_function = get_fruityvice_data(fruit_choice)
-                 streamlit.dataframe(back_from_function)
-    
- streamlit.write('The user entered ', fruit_choice)
+                 streamlit.dataframe(back_from_function)  
 
 # import requests
 
