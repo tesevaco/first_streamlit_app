ee4f1687c7cd67a59d4d15ac298f0f88bef4cec0
tesevaco <120709697+tesevaco@users.noreply.github.com>
2022-12-20 09:38:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
                  back_from_function = get_fruityvice_data(fruit_choice)
                  streamlit.dataframe(back_from_function)
     
-streamlit.write('The user entered ', fruit_choice)
+# streamlit.write('The user entered ', fruit_choice)
 
 # import requests
 
